0496611ef25f572a9c7878a69d8cc4feddd29983
qsh1 <40746505+qsh1@users.noreply.github.com>
2018-08-06 17:48:54 +0800

    Revert "element1.2.0"
    This reverts commit 205d4d288ed6d164e44727e3b8627db305dc8986.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,7 @@
 <template>
   <div id="app">
-
-    <keep-alive>
-      <router-view></router-view>
-    </keep-alive>
-    
-	    <nav class="tabs">
-	      <router-link class="tab" :to="{name: 'home'}">首页</router-link>
-	      <router-link class="tab" :to="{name: 'discover'}">发现</router-link>
-	      <router-link class="tab" :to="{name: 'order'}">订单</router-link>
-	      <router-link class="tab" :to="{name: 'my'}">我的</router-link>
-	    </nav>
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
@@ -21,44 +12,12 @@ export default {
 </script>
 
 <style>
-html, body, #app{
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-}
-.tabs{
-  width: 100%;
-  height: 49px;
-  background: palegoldenrod;
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  display: flex;
-}
-.tabs .tab{
-  flex: 1;
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  line-height: 49px;
-  font-size: 14px;
-}
-.tabs .tab.router-link-active{
-  font-size: 16px;
-  font-weight: bold;
-  color: lightcoral;
-}
-.page{
-  width: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 49px;
-  background: #fff;
-}
-.page.subpage{
-  bottom: 0;
-  z-index: 5;
-}
-.slideInRight, .slideOutRight{
-    animation-duration: 300ms;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.prototype.$center = new Vue();
+Vue.config.productionTip = false
 
-var vm = new Vue({
+/* eslint-disable no-new */
+new Vue({
   el: '#app',
   router,
   components: { App },
